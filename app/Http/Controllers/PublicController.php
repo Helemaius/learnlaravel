d6ade3eda7ac3d341bb555fde36a12f3d2568916
Hele-Mai <hele-mai.kulleste@tptlive.ee>
2021-10-26 15:24:21 +0300

    new files

---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function home(){
+        $name = "Hellu";
+        $array = ['piim', 'sai', 'leib'];
+       return view('home', compact('name', 'array'));
+    }
+
+    public function posts(){
+        return view('posts');
+    }
+}
+
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layout')
+@section('title', 'Posts')
+@section('content')
+    <h1>Posts</h1>
+@endsection
+
+
