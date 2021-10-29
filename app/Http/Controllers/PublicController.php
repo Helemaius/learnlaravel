57bc01ae91f57ff4a3b46ed4dbe571d0bc6dec72
Hele-Mai Kulleste <hele-mai.kulleste@tptlive.ee>
2021-10-29 11:41:32 +0300

    posts page modifiers, button interaction

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,7 +14,10 @@ class PublicController extends Controller
     }
 
     public function posts(){
-        $posts =DB::table('posts')->where('title','LIKE', '%a.')->where('id', '<', 500)->limit(10)->orderBy('title', 'desc')->get();
+        //$posts = Post::select(['title', 'id'])->where('title','LIKE', '%et %')->where('id', '<', 500)->limit(10)->orderBy('title', 'desc')->get();
+//        $skip= request()->get('page') * 16;
+//        $posts = Post::limit(16)->skip($skip)->get();
+        $posts = Post::simplePaginate(16);
 
         return view('posts', compact('posts'));
     }
